3ec497976931c284f986629fac702f73fbd745a2
nhinhiii <nhinguyenyen0908@gmail.com>
2025-09-25 23:51:20 -0400

    finish page.tsx create func to check search and language option logic

--- frontend/src/app/articles/page.tsx
+++ frontend/src/app/articles/page.tsx
@@ -1,8 +1,32 @@
 //this will be the main page
 
+import { Suspense } from "react";
 import { ArticleFilter } from "../components/ArticleFilter";
+import { LoadingSpinner } from "../loading/LoadingSpinner";
+import { ArticleList } from "../components/ArticleList";
+import { Language } from "../type";
+import { getArticle } from "../lib/api";
 
-export default async function ArticlesPage() {
+interface ArticlesPageProps {
+  searchParams?: {
+    search?: string;
+    lang?: Language;
+  };
+}
+export default async function ArticlesPage({
+  searchParams,
+}: ArticlesPageProps) {
+  const articles = await getArticle();
+  const searchTerm = searchParams?.search?.toLowerCase() || "";
+  const selectedLang = searchParams?.lang || "";
+
+  const filteredArticles = articles.filter((article) => {
+    const matchesSearch =
+      article.title.toLowerCase().includes(searchTerm) ||
+      article.preview.toLowerCase().includes(searchTerm);
+    const matchesLang = selectedLang ? article.language === selectedLang : true;
+    return matchesSearch && matchesLang;
+  });
   return (
     <main className="container mx-auto px-4 py-8">
       <div className="text-center mb-10">
@@ -12,11 +36,15 @@ export default async function ArticlesPage() {
         </h1>
         <p className="mt-2 text-lg text-gray-600">
           {" "}
-          Select a news article to begin your journey!!
+          Select a news article to begin your language journey!!
         </p>
       </div>
 
       <ArticleFilter />
+
+      <Suspense fallback={<LoadingSpinner />}>
+        <ArticleList article={filteredArticles} />
+      </Suspense>
     </main>
   );
 }
